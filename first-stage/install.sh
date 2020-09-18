46005935d0d4e22eed7647ab9df7fc128385050b
Liam S. Crouch <spam@petterroea.com>
2020-09-18 19:48:28 +0200

    Add some small fixes that should make the experience better

--- first-stage/install.sh
+++ first-stage/install.sh
@@ -3,3 +3,8 @@
 pacman -Sy --noconfirm base-devel intel-ucode git sudo openssh networkmanager wget curl efibootmgr
 
 systemctl enable NetworkManager
+
+echo "========================================"
+echo "Please uncomment multilib in /etc/pacman.conf before continuing"
+echo "If not, things will NOT work!"
+echo "========================================"
